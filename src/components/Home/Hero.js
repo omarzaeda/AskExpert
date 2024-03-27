4d6ad86b7eb0a4bf25a7d34232b32365f7d76330
omarzaeda <moraeslam1@gmail.com>
2024-03-27 20:55:51 +0200

    last

--- src/components/Home/Hero.js
+++ src/components/Home/Hero.js
@@ -1,5 +1,5 @@
 import React from "react";
-import HeroImage from "../../images/hero-image.jpg";
+import HijabImg from "../../images/HijabImg.jpg";
 import Book from "../../icons/book.svg";
 import Pattern from "../../icons/Pattern.svg";
 const Hero = () => {
@@ -17,11 +17,10 @@ const Hero = () => {
             Hello I'am
           </p>
           <h1 className="text-white text-3xl lg:text-[50px] font-bold font-cabin lg:leading-[58px] my-5 max-w-[502px] w-full">
-            Omar Zaeda <br /> Junior Web Developer
+            Sara Castle<br /> Business Consultant
           </h1>
           <p className="font-lato text-white font-bold">
-            Get help from Alex Moore, a professional business coach with
-            advanced experience on growth and business scaling.
+            This is passionate about delivering growth and the new possibilities it creates for our people, customers and business.
           </p>
           <div className="flex items-center space-x-[11px] font-lato text-gray-1 text-lg mt-[30px] bg-white w-fit rounded-full py-2 px-[14px] cursor-pointer hover:bg-black-5 hover:text-white transition-colors duration-500">
             <img className="w-[25px] lg:w-[43px]" src={Book} alt="" />
@@ -32,7 +31,7 @@ const Hero = () => {
         </div>
       </div>
       <div>
-        <img className="w-full" src={HeroImage} alt="" />
+        <img className="w-full min-h-[600px]" src={HijabImg} alt="" />
       </div>
     </div>
   );
